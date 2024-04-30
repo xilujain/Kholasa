5ef513079c7bc61977fe26803d873139f9357395
xilujain <lujainboo@gmail.com>
2024-04-30 22:32:11 +0300

    try to fix

--- spacy_summarizer.py
+++ spacy_summarizer.py
@@ -1,18 +1,15 @@
 # NLP Pkgs
 import spacy
-# Pkgs for Normalizing Text
 from spacy.lang.en.stop_words import STOP_WORDS
-# Import Heapq for Finding the Top N Sentences
 from heapq import nlargest
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load('en')
 
 
 def text_summarizer(raw_docx):
     raw_text = raw_docx
     docx = nlp(raw_text)
     stopwords = list(STOP_WORDS)
-    # Build Word Frequency # word.text is tokenization in spacy
     word_frequencies = {}
     for word in docx:
         if word.text not in stopwords:
@@ -21,7 +18,7 @@ def text_summarizer(raw_docx):
             else:
                 word_frequencies[word.text] += 1
 
-    maximum_frequency = max(word_frequencies.values(), default=0)
+    maximum_frequency = max(word_frequencies.values())
 
     for word in word_frequencies.keys():
         word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
@@ -42,4 +39,9 @@ def text_summarizer(raw_docx):
     summarized_sentences = nlargest(7, sentence_scores, key=sentence_scores.get)
     final_sentences = [w.text for w in summarized_sentences]
     summary = ' '.join(final_sentences)
-    return summary
+    print("Original Document\n")
+    print(raw_docx)
+    print("Total Length:", len(raw_docx))
+    print('\n\nSummarized Document\n')
+    print(summary)
+    print("Total Length:", len(summary))
